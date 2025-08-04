86a3385126ebe57e540c5a0f4a082302fbb431c2
Mirka McIntyre <mirka.kiana@gmail.com>
2025-08-04 17:41:37 -0400

    Put URL together.

--- js/recipe.js
+++ js/recipe.js
@@ -1,5 +1,5 @@
 // Here is the URL fof the API I'm using
-const url = 'https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/complexSearch?query=side%20salad&diet=vegetarian&intolerances=gluten&includeIngredients=cheese%2Cnuts&excludeIngredients=eggs&instructionsRequired=true&fillIngredients=false&addRecipeInformation=false&addRecipeInstructions=false&addRecipeNutrition=false&maxReadyTime=45&ignorePantry=true&sort=max-used-ingredients&offset=0&number=10';
+const apiUrl = 'https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/complexSearch?query=side%20salad&diet=vegetarian&intolerances=gluten&includeIngredients=cheese%2Cnuts&excludeIngredients=eggs&instructionsRequired=true&fillIngredients=false&addRecipeInformation=false&addRecipeInstructions=false&addRecipeNutrition=false&maxReadyTime=45&ignorePantry=true&sort=max-used-ingredients&offset=0&number=10';
 
 // My API key
 const apiKey = b209950e56msh44e6c010fffefe1p10300ajsne45cf0a3b310;
@@ -19,5 +19,7 @@ function fetchResults(event) {
     event.preventDefault();
 }
 
-
+// Putting together the URL
+const url = `${apiUrl}?q=${searchInput.value}side%20salad&diet=vegetarian&intolerances=gluten&includeIngredients=cheese%2Cnuts&excludeIngredients=eggs&instructionsRequired=true&fillIngredients=false&addRecipeInformation=false&addRecipeInstructions=false&addRecipeNutrition=false&maxReadyTime=45&ignorePantry=true&sort=max-used-ingredients&offset=0&number=10')`;
+console.log(url);
 
