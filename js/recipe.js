7e7a8a7bdaedeaee9ebdf26f5ec3fcdaf8a5272f
Mirka McIntyre <mirka.kiana@gmail.com>
2025-08-04 16:49:39 -0400

    Targeted DOM elements.

--- js/recipe.js
+++ js/recipe.js
@@ -3,3 +3,9 @@ const url = 'https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes
 
 // My API key
 const apiKey = b209950e56msh44e6c010fffefe1p10300ajsne45cf0a3b310;
+
+
+// Targeting DOM elements
+const searchInput = document.querySelector(".search");
+const submitButton = document.querySelector(".submit");
+const article = document.querySelector("article");
