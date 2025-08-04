7382026c226efcab0e11b31fd4c0f19afa8de7bd
Mirka McIntyre <mirka.kiana@gmail.com>
2025-08-04 16:26:30 -0400

    Added files, url and API key.

---
+++ js/recipe.js
@@ -0,0 +1,5 @@
+// Here is the URL fof the API I'm using
+const url = 'https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/complexSearch?query=side%20salad&diet=vegetarian&intolerances=gluten&includeIngredients=cheese%2Cnuts&excludeIngredients=eggs&instructionsRequired=true&fillIngredients=false&addRecipeInformation=false&addRecipeInstructions=false&addRecipeNutrition=false&maxReadyTime=45&ignorePantry=true&sort=max-used-ingredients&offset=0&number=10';
+
+// My API key
+const apiKey = b209950e56msh44e6c010fffefe1p10300ajsne45cf0a3b310;
